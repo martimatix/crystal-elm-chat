d8f3adff5c9dc90628635090b0ec93094f548bcc
Mario Martinez <zenitram.oiram@gmail.com>
2016-10-03 09:16:27 +1100

    Initial commit

---
+++ src/kemal-chat-elm.cr
@@ -0,0 +1,24 @@
+require "kemal"
+
+SOCKETS = [] of HTTP::WebSocket
+
+# get "/" do
+#   render "views/index.ecr"
+# end
+
+ws "/chat" do |socket|
+  # Add the client to SOCKETS list
+  SOCKETS << socket
+
+  # Broadcast each message to all clients
+  socket.on_message do |message|
+    SOCKETS.each { |socket| socket.send message}
+  end
+
+  # Remove clients from the list when it's closed
+  socket.on_close do
+    SOCKETS.delete socket
+  end
+end
+
+Kemal.run
---
+++ src/kemal-chat-elm/version.cr
@@ -0,0 +1,3 @@
+module Kemal::Chat::Elm
+  VERSION = "0.1.0"
+end
